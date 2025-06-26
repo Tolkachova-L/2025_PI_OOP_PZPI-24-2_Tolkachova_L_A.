1632bf029ce723648df018643dce638bc89bd0dc
Tolkachova-L <liliia.tolkachova@nure.ua>
2025-06-26 06:06:12 +0300

    Update MainMenu.cs

--- ConsoleApp3/MainMenu/MainMenu.cs
+++ ConsoleApp3/MainMenu/MainMenu.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp3
         protected override Dictionary<string, IPage> Options { get; }
         protected override string Title => "Щоденник\n" +
             "(Рекомендуємо натиснути F1 та прочитати основнi функцiї кнопок)\n";
-            protected override string Title => GetClosestNote();
+        protected override string Footer => GetClosestNote();
         private NoteManager _noteManager = new NoteManager();
         public MainMenu()
         {
